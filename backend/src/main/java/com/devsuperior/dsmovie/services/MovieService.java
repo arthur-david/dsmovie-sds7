4cbb0412d16eb8a649428c5173ab962a2deb1512
arthur-david <arthurdavid000@gmail.com>
2022-03-12 01:42:37 -0300

    Find movies

---
+++ backend/src/main/java/com/devsuperior/dsmovie/services/MovieService.java
@@ -0,0 +1,29 @@
+package com.devsuperior.dsmovie.services;
+
+import com.devsuperior.dsmovie.dto.MovieDTO;
+import com.devsuperior.dsmovie.entities.Movie;
+import com.devsuperior.dsmovie.repositories.MovieRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class MovieService {
+
+    private final MovieRepository movieRepository;
+    
+    @Transactional(readOnly = true)
+    public Page<MovieDTO> findAll(Pageable pageable) {
+        Page<Movie> result = movieRepository.findAll(pageable);
+        return result.map(x -> new MovieDTO(x));
+    }
+    
+    @Transactional(readOnly = true)
+    public MovieDTO findById(Long id) {
+        Movie movie = movieRepository.findById(id).get();
+        return new MovieDTO(movie);
+    }
+}
