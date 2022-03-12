c402325cf4179d71fbe7f634d5bd2746d401480d
arthur-david <arthurdavid000@gmail.com>
2022-03-12 00:55:02 -0300

    Domain model, database seed

---
+++ backend/src/main/java/com/devsuperior/dsmovie/entities/Score.java
@@ -0,0 +1,29 @@
+package com.devsuperior.dsmovie.entities;
+
+import javax.persistence.EmbeddedId;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter @Setter
+@NoArgsConstructor
+@Entity
+@Table(name = "tb_score")
+public class Score {
+    
+    @EmbeddedId
+    private ScorePK id = new ScorePK();
+
+    private Double value;
+
+    public void setMovie(Movie movie) {
+        id.setMovie(movie);
+    }
+
+    public void setUser(User user) {
+        id.setUser(user);
+    }
+}
